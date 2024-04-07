c1ef12a428456b3685c360c53ceece5e4c684479
Pratip Ghosh <pratip.ghosh@capgemini.com>
2024-04-07 18:03:27 +0530

    Updated GA file.

--- web/modules/custom/d10_core/src/Controller/D10CoreController.php
+++ web/modules/custom/d10_core/src/Controller/D10CoreController.php
@@ -4,11 +4,17 @@ namespace Drupal\d10_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * The custom controller to play with Drupal.
+ */
 class D10CoreController extends ControllerBase {
 
+  /**
+   * A standard markup returning route callback.
+   */
   public function playWithMe() {
     return [
-      '#markup' => 'Are you here to play with me?'
+      '#markup' => 'Are you here to play with me?',
     ];
   }
 
